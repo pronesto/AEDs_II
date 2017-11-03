7016ad849ca0eaec2113b312e730476d5a636b29
Fernando <pronesto@gmail.com>
2017-11-03 12:09:02 -0200

    Hashset.

---
+++ HashTables/HashSet.c
@@ -0,0 +1,66 @@
+/*
+ * This file implements a Set datatype, using an integer array to index the
+ * elements. 
+ *
+ * Author: fernando@dcc.ufmg.br
+ * Date: November 3rd, 2017
+ */
+
+#include <stdlib.h>
+
+#include "Set.h"
+
+struct node {
+  Key key;
+  struct node *next;
+};
+
+struct set_concrete {
+  struct node **table;
+  size_t size;
+};
+
+#define TABLE_SIZE 0xFFFF
+
+void initialize(Set* s) {
+  *s = (Set)malloc(sizeof(struct set_concrete));
+  (*s)->table = (struct node**)calloc(TABLE_SIZE, sizeof(struct node*));
+}
+
+size_t hash(Key element) {
+  return element & TABLE_SIZE;
+}
+
+void insert(Set s, Key element) {
+  struct node* it;
+  size_t h = hash(element);
+  it = malloc(sizeof *it);
+  it->key = element;
+  it->next = s->table[h];
+  s->table[h] = it;
+}
+
+int contains(Set s, Key element) {
+  size_t h = hash(element);
+  if (s->table[h]) {
+    struct node *it = s->table[h];
+    while (it) {
+      if (it->key == element)
+        return 1;
+    }
+  }
+  return 0;
+}
+
+void close(Set s) {
+  for (int i = 0; i < TABLE_SIZE; i++) {
+    struct node *j = s->table[i];
+    while (j) {
+      struct node *aux = j;
+      j = j->next;
+      free(aux);
+    }
+  }
+  free(s->table);
+  free(s);
+}
