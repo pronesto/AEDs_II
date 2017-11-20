b116094813742f07295b4c4a1b0decb0c8c5ac8b
Fernando <pronesto@gmail.com>
2017-11-20 10:49:38 -0200

    Adding script to time the benchmarks.

--- HashTables/timeSet.bash
+++ HashTables/timeSet.bash
@@ -11,42 +11,30 @@ if [ $# -lt 3 ]
 then
   echo "Syntax: timeTree min_nodes max_nodes stp_nodes"
   echo "Default values: min_nodes = 100, max_nodes = 110101, stp_nodes = 10000"
-  min_nodes=1000
-  max_nodes=110100
-  stp_nodes=10000
+  min=1000
+  max=110100
+  stp=10000
 else
-  min_nodes=$1
-  max_nodes=$2
-  stp_nodes=$3
-  echo "min_nodes = $min_nodes, max_nodes = $max_nodes, stp_nodes = $stp_nodes"
+  min=$1
+  max=$2
+  stp=$3
+  echo "min_nodes = $min, max_nodes = $max, stp_nodes = $stp"
 fi
 
-echo "int_sort, Time (s)"
-for ((num_nodes = $min_nodes; num_nodes < $max_nodes; num_nodes += $stp_nodes))
-do
-  ./IntArraySet $num_nodes $num_nodes ;
-done
+run_test() {
+  echo "$1, Num-Nodes"
+  for ((num_nodes = $min; num_nodes < $max; num_nodes += $stp))
+  do
+    ./$1 $num_nodes $num_nodes
+  done
+}
 
-echo "bit_sort, Time (s)"
-for ((num_nodes = $min_nodes; num_nodes < $max_nodes; num_nodes += $stp_nodes))
-do
-  ./BitArraySet $num_nodes $num_nodes ;
-done
+clang -o IntArraySet CompSetMain.c IntArraySet.c -Iinclude
+run_test IntArraySet
 
-echo "rbt_sort, Time (s)"
-for ((num_nodes = $min_nodes; num_nodes < $max_nodes; num_nodes += $stp_nodes))
-do
-  ./RBTSet $num_nodes $num_nodes ;
-done
+clang RedBlackSet.c RBT.c CompSetMain.c -o RBTMain -Iinclude
+run_test RBTSet
 
-echo "hst_sort, Time (s)"
-for ((num_nodes = $min_nodes; num_nodes < $max_nodes; num_nodes += $stp_nodes))
-do
-  ./HashSet $num_nodes $num_nodes ;
-done
-
-echo "neq_sort, Time (s)"
-for ((num_nodes = $min_nodes; num_nodes < $max_nodes; num_nodes += $stp_nodes))
-do
-  ./NoEqualHashSet $num_nodes $num_nodes ;
-done
+# run_test BitArraySet
+# run_test HashSet
+# run_test NoEqualHashSet
